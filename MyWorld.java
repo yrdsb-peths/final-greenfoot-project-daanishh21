c077c34922c90a9c926742e7d4baca8248256ad1
daanishh21 <117941519+daanishh21@users.noreply.github.com>
2023-01-10 17:24:19 -0500

    trying to add animation fire on rocket

--- MyWorld.java
+++ MyWorld.java
@@ -8,18 +8,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        
         super(600, 600, 1);
         
         Rocket rocket = new Rocket();
-        addObject(rocket, 300, 300);
-        
+        addObject(rocket, 100, 300);
     }
 }
