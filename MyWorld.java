8ad4c73146b7a1b3ff43c1141358feed0d0b2be0
daanishh21 <117941519+daanishh21@users.noreply.github.com>
2023-01-11 13:39:50 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,7 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // adding rocket to the world
         super(600, 600, 1);
         
         Rocket rocket = new Rocket();
